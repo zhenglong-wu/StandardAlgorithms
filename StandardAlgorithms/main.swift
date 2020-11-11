83dfcd56ca425bab11f5e82bf746e2cb84be6c4f
Zhenglong Wu <wu.z@etoncollege.org.uk>
2020-11-11 11:02:48 +0000

    Initial commit of project.

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Wu, Zhenglong (SPH) on 11/11/2020.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let myArray = [1, 2, 3, 6, 7, 3, 7]
+
+print(myArray)
+
