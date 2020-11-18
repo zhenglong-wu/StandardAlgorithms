f6c34076ee983e8e5aef17345ad7d1ec765a77d7
Zhenglong Wu <wu.z@etoncollege.org.uk>
2020-11-18 15:37:14 +0000

    Push to restart compiler

--- StandardAlgorithms/Searching.swift
+++ StandardAlgorithms/Searching.swift
@@ -47,5 +47,4 @@ class Searching {
         return false
     }
     
-    
 }
--- StandardAlgorithmsTest/SearchingTest.swift
+++ StandardAlgorithmsTest/SearchingTest.swift
@@ -5,6 +5,21 @@
 //  Created by Zhenglong Wu on 17/11/2020.
 //
 
-import Foundation
+import XCTest
 
-
+class SearchingTest: XCTestCase {
+    
+    func testLinearSearchWithTargetInArray() {
+        
+        let searching = Searching()
+        
+        let expected = true
+        
+        let result = searching.linearSearch(data: [], target: 5)
+        
+        XCTAssertEqual(result, expected)
+        
+        
+        
+    }
+}
