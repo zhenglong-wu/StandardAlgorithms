8207c12dc6ec7f332df32dc2058d2586a4711c54
Zhenglong Wu <wu.z@etoncollege.org.uk>
2020-11-23 21:48:02 +0000

    Main.swift updated

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -99,14 +99,14 @@ func menu() {
                 print("""
                     Insertion Sort:
                         
-                    Description: Insertion Sort is a divide and conquer type algorithm, it repeatedly
-                                    halves the list down until all the items are in its individual,
-                                    single sublist. It now starts to sort them with its surrounding
-                                    sublists and starts to merge the sorted list, once the list start
-                                    to merge each indexed element of each sublist is compared to other
-                                    same indexed elements and are sorted into a bigger list. This
-                                    process is repeated until there is only one list.
-                    
+                    Description: Insertion Sort starts off by comparing the first and second element
+                                    of the list, then creates a sorted sublist that it repeated compares
+                                    and sorts, taking elements from the unsorted list from the begining.
+                                    When it takes an element from the unsorted list, it compares that
+                                    element with the existing elements of the sorted list and inserts
+                                    that element at the correct position. When the unsorted list is
+                                    empty, the entirety of the list is sorted.
+
                     Big O Complexities:
                         
                         Best case scenario: time complexity - O(n), space complexity - O(1)
@@ -126,6 +126,41 @@ func menu() {
                 print("Input array - \(data)")
                 
                 print("\nOutput array - \(sorting.insertionSort(data: data))")
+                
+            case 4:
+                
+                let sorting = Sorting()
+                
+                print("""
+                    Quick Sort:
+                        
+                    Description: Quick Sort starts off by comparing the first and second element
+                                    of the list, then creates a sorted sublist that it repeated compares
+                                    and sorts, taking elements from the unsorted list from the begining.
+                                    When it takes an element from the unsorted list, it compares that
+                                    element with the existing elements of the sorted list and inserts
+                                    that element at the correct position. When the unsorted list is
+                                    empty, the entirety of the list is sorted.
+
+                    Big O Complexities:
+                        
+                        Best case scenario: time complexity - O(n), space complexity - O(1)
+
+                        Average case scenario: time complexity - O(n^2), space complexity - O(1)
+
+                        Worst case scenario: time complexity - O(n^2), space complexity - O(1)
+                            
+                    """)
+                
+                var data = [Int]()
+                
+                for _ in 1...25{
+                    data.append(Int.random(in: 1..<100))
+                }
+                
+                print("Input array - \(data)")
+                
+                print("\nOutput array - \(sorting.quickSort(data: data))")
                 
             default:
                 print("")
