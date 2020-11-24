21f2586183fb01ce1d8f2f6693f8b410a2e7b2b9
Zhenglong Wu <wu.z@etoncollege.org.uk>
2020-11-24 00:47:44 +0000

    Finished Standard Algorithms and Unknown Algorithms

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -134,21 +134,22 @@ func menu() {
                 print("""
                     Quick Sort:
                         
-                    Description: Quick Sort starts off by comparing the first and second element
-                                    of the list, then creates a sorted sublist that it repeated compares
-                                    and sorts, taking elements from the unsorted list from the begining.
-                                    When it takes an element from the unsorted list, it compares that
-                                    element with the existing elements of the sorted list and inserts
-                                    that element at the correct position. When the unsorted list is
-                                    empty, the entirety of the list is sorted.
+                    Description: Quick Sort is also a divide and conquer type algorithm. It recursively
+                                    calls a function which chooses an element in the list as a pivot,
+                                    then compares all the items in the list to the pivot, and filters
+                                    all smalller numbers to the "left" array, and all the larger
+                                    numbers to the "right" array. Then the algorithm recursively calls
+                                    the function that does the left and right sorting for each left and
+                                    right array. At the base case the algorithm combines all the "left"
+                                    and "right" arrays to form a sorted array.
 
                     Big O Complexities:
                         
-                        Best case scenario: time complexity - O(n), space complexity - O(1)
+                        Best case scenario: time complexity - O(n * log n), space complexity - O(n)
 
-                        Average case scenario: time complexity - O(n^2), space complexity - O(1)
+                        Average case scenario: time complexity - O(n * log n), space complexity - O(n)
 
-                        Worst case scenario: time complexity - O(n^2), space complexity - O(1)
+                        Worst case scenario: time complexity - O(n^2), space complexity - O(n)
                             
                     """)
                 
@@ -162,6 +163,82 @@ func menu() {
                 
                 print("\nOutput array - \(sorting.quickSort(data: data))")
                 
+            case 5:
+                
+                let searching = Searching()
+                
+                print("""
+                    Linear Search:
+                        
+                    Description: Linear Search is a brute force algorithm that iterates through
+                                    a list and checks for the target integer one item by one item
+                                    until the target item is found, if the target item is not found
+                                    and the algorithm reaches the end of the list, the algorithm returns
+                                    that the target was not found in the algorithm.
+
+                    Big O Complexities:
+                        
+                        Best case scenario: time complexity - O(n), space complexity - O(1)
+
+                        Average case scenario: time complexity - O(n), space complexity - O(1)
+
+                        Worst case scenario: time complexity - O(n), space complexity - O(1)
+                            
+                    """)
+                
+                var data = [Int]()
+                
+                let target: Int = 8
+                
+                for _ in 1...25{
+                    data.append(Int.random(in: 1..<10))
+                }
+                
+                print("Target - \(target)")
+                
+                print("Input array - \(data)")
+                
+                print("\nOutput array - \(searching.linearSearch(data: data, target: target))")
+                
+            case 6:
+                
+                let searching = Searching()
+                
+                print("""
+                    Binary Search:
+                        
+                    Description: Binary Search is a decrease and conquer algorithm that uses "flags".
+                                    It first finds the item at the middle of the list and compares it
+                                    with the target. If the middle item is bigger then it goes below the
+                                    middle item of the list, if the middle item is smaller than the target
+                                    then it goes above the middle item of the list. The algorithm repeatedly
+                                    does this, finding the middle item of the sub-lists and comparing until
+                                    it finds its target.
+
+                    Big O Complexities:
+                        
+                        Best case scenario: time complexity - O(1), space complexity - O(1)
+
+                        Average case scenario: time complexity - O(log n), space complexity - O(1)
+
+                        Worst case scenario: time complexity - O(log n), space complexity - O(1)
+                            
+                    """)
+                
+                var data = [Int]()
+                
+                let target: Int = 8
+                
+                for _ in 1...25{
+                    data.append(Int.random(in: 1..<10))
+                }
+                
+                print("Target - \(target)")
+                
+                print("Input array - \(data)")
+                
+                print("\nResult - \(searching.binarySearch(data: data, target: target))")
+            
             default:
                 print("")
             }
